9195a56b7b0b58da38ae056e8dc98ab052d0fcff
Lucy Wilson <lucyleleu@googlemail.com>
2018-09-24 14:43:27 +0000

    login and signup

--- app.py
+++ app.py
@@ -14,9 +14,40 @@ mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/login')
+def index():
+    if 'username' in session:
+        return "You are logged in as " + session['username']
+    return render_template('index.html')
+
+# Login and Signup inc password encryption code adapted from PrettyPrinted tutorial: https://github.com/PrettyPrinted/mongodb-user-login 
+# https://www.youtube.com/watch?v=vVx1737auSE
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template("login.html")
+    users = mongo.db.users
+    login_user = users.find_one({"username" : request.form['username']})
+    
+    if login_user:
+        if bcrypt.hashpw(request.form['password'].encode('utf-8'), 
+        login_user['password']) == login_user['password'].encode('utf-8'):
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+    return "Invalid username/password combination"
+        
+        
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        users = mongo.db.users
+        existing_user = users.find_one({"username": request.form['username']})
+        
+        if existing_user is None:
+            hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
+            users.insert({"username": request.form['username'],"email": request.form['email'], "password" : hashpass })
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        return 'That username already exists!'
+        
+    return render_template('signup.html')
 
 
 

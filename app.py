76770672e2c7bb143dd11a9663c1ac18ddff3876
Lucy Wilson <lucyleleu@googlemail.com>
2018-09-26 12:37:27 +0000

    add recipe - fault with levers

--- app.py
+++ app.py
@@ -17,24 +17,25 @@ mongo = PyMongo(app)
 def index():
     #if 'username' in session:
         #return "You are logged in as " + session['username']
-    return render_template('index.html', breakfast_recipes=mongo.db.recipes.find({"category": "Breakfast"}))
+    return render_template('index.html', 
+    breakfast_recipes=mongo.db.recipes.find({"category": "Breakfast"}),
+    lunch_recipes=mongo.db.recipes.find({"category": "Lunch"}))
 
 # Login and Signup inc password encryption code adapted from PrettyPrinted tutorial: https://github.com/PrettyPrinted/mongodb-user-login 
 # https://www.youtube.com/watch?v=vVx1737auSE
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
+    users = mongo.db.users
+    login_user = users.find_one({'username' : request.form['username']})
     if request.method == 'POST':
-        users = mongo.db.users
-        login_user = users.find_one({"username" : request.form['username']})
-    
         if login_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), 
-            login_user['password']) == login_user['password'].encode('utf-8'):
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
                 session['username'] = request.form['username']
                 return redirect(url_for('index'))
-        return "Invalid username/password combination"
-        
-    return render_template('login.html')    
+    
+        return 'Invalid username/password combination'
+    return render_template('login')    
         
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
@@ -53,12 +54,41 @@ def signup():
 
 @app.route('/addrecipe', methods=['POST', 'GET'])
 def addrecipe():
+    
     if 'username' in session:
+        if request.method == 'POST':
+            recipes = mongo.db.recipes
+            
+            ingredient_list = []
+            ingredients = request.form['ingredients'].splitlines()
+            ingredient_list.append(ingredients)
+            
+            method_list = []
+            method = request.form['method'].splitlines()
+            method_list.append(method)
+            
+            print(ingredient_list)
+            recipes.insert({"title": request.form['title'],
+                            "category": request.form['category'],
+                            "author": session['username'],
+                            "description": request.form['description'],
+                            "ingredients": ingredient_list,
+                            "method": method_list,
+                            "likes": "0",
+                            "vegetarian": request.form['vegetarian'],
+                            "vegan": request.form['vegetarian']
+            })
+            return render_template('index.html')
+        
         return render_template("addrecipe.html")
 
     return "please login to add a recipe"        
 
-
+@app.route('/recipe/<recipe_id>', methods=['POST', 'GET'])
+def recipe(recipe_id):
+    the_recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+   
+    return render_template('recipe.html', recipe=the_recipe)
 
     
 if __name__ == '__main__':

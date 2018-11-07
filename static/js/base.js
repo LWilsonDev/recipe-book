4e290121f27c6b2323403b8c2b181ddaa5b7ab50
Lucy Wilson <lucyleleu@googlemail.com>
2018-11-07 19:31:13 +0000

    remove js from base.html and create base.js file

---
+++ static/js/base.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+      //Materialize.css Jquery
+      $('.collapsible').collapsible();
+      $('select').material_select();
+      $(".button-collapse").sideNav();
+      $(".dropdown-button").dropdown();
+      $('.modal').modal({
+        dismissible: true,
+        opacity: .5,
+        inDuration: 300,
+        outDuration: 200,
+        startingTop: '4%',
+        endingTop: '10%',
+      });
+      //Materialize JQuery END
+
+      // Add recipe category validation
+      $('.js_recipe_btn').click(function() {
+        if (!$('.category_wrapper li').hasClass('selected'))
+          alert("Please select a category for your recipe");
+        if (!$('.vege_wrapper li').hasClass('selected'))
+          alert("Please select an option to indicate if your recipe is suitable for vegetarians");
+      });
+      
+     $('#myTable').pageMe({
+    pagerSelector:'#myPager'});
+});
